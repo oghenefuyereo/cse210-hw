e2adbeb150e0a8be2cab6a8d74ede407f7c048a8
oghenefuyereo <oghenefuyereo@gmail.com>
2024-01-14 21:13:48 -0500

    Program.cs csharp-pre/prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        Console.WriteLine("What is your first namefff?");
     }
 }
